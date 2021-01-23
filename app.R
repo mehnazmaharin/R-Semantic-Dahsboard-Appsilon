252c654822ab2f53647d101c4bfe9b0bd1cfd09c
mehnazmaharin <mehnazmaharin@gmail.com>
2021-01-22 19:07:20 -0500

    made more changes

--- app.R
+++ app.R
@@ -11,13 +11,13 @@ library(shiny)
 library(dplyr)
 library(leaflet)
 library(data.table)
-# Define UI for application that draws a histogram
+# #
 if (interactive()){
   library(shiny)
   library(shiny.semantic)
   shipdata<-fread("/Users/mehnazmaharin/Documents/R-Semantic-Dahsboard-Appsilon/ships.csv",
                      header=TRUE)
-  #NOTE from MM: ensure that you change above path and unzip the zipped data
+  #NOTE from Mehnaz: ensure that you change above path and unzip the zipped data
   vessel_type <- shipdata %>% pull(ship_type) %>% unique
   vessel_name <- shipdata %>% pull(SHIPNAME) %>% unique
   
